7bb41036d869855c9b3cc3e7b1f7706e5e85f257
João Victor A <jvas1999@gmail.com>
2021-11-19 19:53:11 -0300

    uo
    up

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Footer.css'
+
+export default function Footer(){
+    return(
+        <footer>
+            <h2>Galeria</h2>
+            <span>As mais belas imagens</span>
+            <br/>
+            <span>Copyright &copy;</span>
+        </footer>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css'
+
+export default function Header(){
+    return(
+        <header data-testid="header">
+            <a href="#">Home</a>
+            <a href="#">Perfil</a>
+        </header>
+    )
+}
